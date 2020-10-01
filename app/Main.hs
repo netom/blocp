d77c6c10385b2e3d12cb0d02573f2f8ff2f80402
Fábián Tamás László <giganetom@gmail.com>
2020-10-01 16:20:42 +0200

    Command line options, TODO list

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,58 @@
 module Main where
 
 import Lib
+import Options.Applicative
+
+data Options = Options
+    { optSource :: String
+    , optTarget :: String
+    , optIgnoreLengthMismatch :: Bool
+    } deriving Show
+
+options :: ParserInfo Options
+options = info
+    (helper <*> (
+        Options
+            <$> (  strOption
+                $  long "source"
+                <> short 's'
+                <> metavar "SOURCE"
+                <> help "The source file."
+                )
+            <*> (  strOption
+                $  long "target"
+                <> short 't'
+                <> metavar "TARGET"
+                <> help "The target file."
+                )
+            <*> (  switch 
+                $ long "ignore-length-mismatch"
+                <> short 'l'
+                <> help "Don't abort if the source and target size does not match."
+                )
+    )) (
+        fullDesc
+            <> progDesc "Copy modified blocks between large files or block devices."
+    )
 
 main :: IO ()
-main = someFunc
+main = do
+    -- TODO Features:
+    --
+    -- Tunable read, write and compare block sizes
+    -- Offset / length (e.g. for copying partitions)
+    -- Ignore size mismatch target
+    -- Append data to the target
+    -- Synchronous writes
+    -- Periodic sync
+    -- Retries on read/write errors
+    -- Failure modes on errors
+    --   Fail fast
+    --   Retry N times
+    --   Retry quickly or later
+    --   Write error log
+
+    -- Check: file existance
+    -- Check: files must be of the same size
+    _ <- execParser options
+    putStrLn "TODO"
